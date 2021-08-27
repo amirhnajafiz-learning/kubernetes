ecfb20bc71cd540be67bc290a4df74b0908b98bd
9831065 <najafizadeh21@gmail.com>
2021-08-27 15:01:53 +0430

    Task controller added

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+}

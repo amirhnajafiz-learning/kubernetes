781313d56a26b028f607e7467fc8fb81e137a1f2
9831065 <najafizadeh21@gmail.com>
2021-08-27 19:54:58 +0430

    Task controller codes added

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,22 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $tasks = Task::query()->where($request->get('id', 1))->get();
+        return view('index')
+            ->with('tasks', $tasks);
     }
 
     public function create()
     {
-        //
+        return view('index');
     }
 
     public function store(Request $request)
     {
-        //
+        $task = Task::query()->create($request->all());
+        return redirect('task.index')
+            ->with('status', 'OK');
     }
 }

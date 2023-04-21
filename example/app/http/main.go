377ff9a3f316f57bef1bf8642f7502af448803c6
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-21 17:29:36 +0330

    add: env into main

--- example/app/http/main.go
+++ example/app/http/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("welcome"))
+		_, _ = w.Write([]byte(os.Getenv("MESSAGE")))
 	})
 
 	log.Println("http server is on 80 ...")
